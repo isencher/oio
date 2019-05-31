5ad7f01516fc0d3945600f6a4189fd652657a35d
wanxyoo <isencher@outlook.com>
2019-05-31 09:12:15 +0800

    split lib into lib.net and lib.core

---
+++ ting/lib.net/cnn/cnnSet.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ting.lib;
+
+namespace ting.lib
+{
+    public partial class cnnSet : Form
+    {
+
+        private cnnString _cnn = new cnnString();
+
+        public cnnSet()
+        {
+            InitializeComponent();
+            cboIntegratedSecurity.SelectedIndexChanged += (sender, e) => {
+                if (this.cboIntegratedSecurity.Text == this.cboIntegratedSecurity.Items[1].ToString()) //选择SQL Server身份验证
+                {
+                    this.txtUserId.Enabled = true; this.txtPwd.Enabled = true; this.txtUserId.Focus();
+                }
+                else
+                {
+                    this.txtUserId.Enabled = false; this.txtPwd.Enabled = false;
+                }
+
+            };
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            #region 有效性验证
+            if (this.txtServer.Text.Trim() == "")
+            {
+                MessageBox.Show("必须输入服务器名或服务器的IP地址！", "验证提示", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                this.txtServer.Focus(); return;
+            }
+            if (this.txtDatabase.Text.Trim() == "")
+            {
+                MessageBox.Show("必须输入数据库名！", "验证提示", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                this.txtDatabase.Focus(); return;
+            }
+            if (this.cboIntegratedSecurity.SelectedIndex == 1)
+            {
+                if (this.txtUserId.Text.Trim() == "")
+                {
+                    MessageBox.Show("必须输入用户名！", "验证提示", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    this.txtUserId.Focus(); return;
+                }
+                if (this.txtPwd.Text.Trim() == "")
+                {
+                    if (MessageBox.Show("确定使用空密码吗？", "验证提问", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                    {
+                        this.txtPwd.Focus(); return;
+                    }
+                }
+            }
+
+            #endregion
+
+            #region 生成cnnString实体
+            _cnn.DataSource = txtServer.Text.Trim();
+            _cnn.InitialCatalog = txtDatabase.Text.Trim();
+            if (cboIntegratedSecurity.SelectedIndex == 0)
+            {
+                _cnn.IntegratedSecurity = "True";
+                _cnn.UserId = null;
+                _cnn.Password = null;
+            }
+            else
+            {
+                _cnn.IntegratedSecurity = "False";
+                _cnn.UserId = txtUserId.Text.Trim();
+                _cnn.Password = txtPwd.Text.Trim();
+            }
+
+            #endregion
+
+            #region 保存到本地磁盘
+            string name = (txtName.Text.ToString() == "") ? "default" : txtName.Text.ToString();
+            bool result = withCnn.UpdateConnectionString(name, new cnnString().ConvertcnnString(_cnn));
+            if (result)
+            {
+                MessageBox.Show("保存成功！", "操作提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("保存失败！请向管理员反馈，以查明原因。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            #endregion
+
+            //退出对话框
+            this.Close();
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
---
+++ ting/lib.net/withCnn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ting.lib
+{
+    public class withCnn
+    {
+        private static rwcnnTofile rw = new rwcnnTofile();
+        //private static sec se = new sec();
+        private static string password = "@*:'~`t/|]t[[2+-";
+        private static string file = "set.config";
+        private static cnnString cs = new cnnString();
+
+        /// <summary>
+        /// to update a cnnstring string of cnn name into set.config file
+        /// </summary>
+        /// <param name="name">cnn name</param>
+        /// <param name="strcnn">cnnstring string</param>
+        /// <returns>success or fail</returns>
+        public static bool UpdateConnectionString(string name, string strcnn)
+        {
+            // encrypt cnnstring string
+            strcnn = sec.Encrypt(strcnn, password);
+            return rw.UpdateConnectionString(name, strcnn, true, file);
+        }
+
+        /// <summary>
+        /// to get a cnnstring string by cnn name from set.config file
+        /// </summary>
+        /// <param name="name">cnn name</param>
+        /// <returns>return a cnnstring string, return null when set.config is not exist or cnnstring string from file is invalid.</returns>
+        public static string GetConnectionString(string name)
+        {
+
+            var result = rw.GetConnectionString(name, file);
+            if (result != null)
+            {
+                // descrypt cnnstring string
+                result = sec.Decrypt(result, password);
+                if (rw.ValidateConnectionString(result))
+                {
+                    return result;
+                }
+                else { return null; }
+            }
+            else { return null; }
+        }
+
+        public static Form SetCnnUI
+        {
+            get { return new cnnSet(); }
+        }
+    }
+}
