5196c06584a405e9f58766a8187455914f88e69d
wanxyoo <isencher@outlook.com>
2019-06-01 17:16:16 +0800

    create dal library for .net

--- ting/model.net/Operation.cs
+++ ting/model.net/Operation.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ting.model
+namespace ting.model.net
 {
     [Table("operation", Schema = "syst")]
     [Serializable]
